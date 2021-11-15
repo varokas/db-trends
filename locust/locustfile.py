2d36c92a4e61c99787ea991418ca4b72d68771aa
varokas <varokas@panusuwan.com>
2021-11-16 05:50:48 +0700

    Update locustfile.py

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,10 +1,10 @@
 from locust import HttpUser, task
 import random
 
-num_owners = 10
+num_owners = 20
 owners = [ f"locust-{i}" for i in range(num_owners) ]
-num_blocks = 500
-under = 5
+num_blocks = 20
+under = 10
 over = 1
 
 def get_new_counter(counter): 
@@ -20,10 +20,14 @@ def get_new_counter(counter):
 class BookingUser(HttpUser):
     @task
     def booking(self):
-        with self.client.get("/api/booking", name="/api/booking") as res:
+        with self.client.get("/api/booking", name="/api/booking", catch_response=True) as res:
             res_json = res.json()
 
             #sorted_tuples = sorted(res_json, key=lambda b: b["counter"] if b["counter"] else 0)
+            if res.status_code != 200:
+              res.failure("Error getting booking: " + str(res.status_code))
+              return
+
             selected_blocks = random.sample(res_json, num_blocks)
             owner = random.choice(owners)
 
